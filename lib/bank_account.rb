a99bc878ed8205cc54500c78ee0999d22d1a8b0c
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 21:46:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,4 +6,20 @@ class BankAccount
     @balance = balance
     @status = status
   end
+
+  def deposit
+
+  end
+
+  def display_balance
+
+  end
+
+  def valid?
+
+  end
+
+  def close_account
+
+  end
 end
