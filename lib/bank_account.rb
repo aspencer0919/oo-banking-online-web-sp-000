cee1df572b680079edd5aeeb6d5cf1ad20b532a6
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 21:44:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,9 @@
 class BankAccount
+  attr_reader :name, :balance, :staus
 
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
 end
